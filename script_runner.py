b94f5f725836c8dfccae8c70d1cfbdd6649a8231
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-01-21 14:33:59 +0200

    + sequential execution mode

--- script_runner.py
+++ script_runner.py
@@ -10,13 +10,22 @@ MONGODB_PACKAGE = "pymongo[srv]"
 
 
 def main():
-    if len(sys.argv) != 3:
-        print('usage script_runner.py: <integration tests path> <api token>')
+    if len(sys.argv) < 3:
+        print('usage script_runner.py: <integration tests path> <api token> <optional: parallel mode>')
         exit(1)
 
     install_dependencies()
-    if not run_all_test_scripts(sys.argv[1], sys.argv[2]):
-        exit(1)
+
+    parallel_mode = True
+    if len(sys.argv == 4):
+        parallel_mode = sys.argv[3].lower() == 'true'
+
+    if parallel_mode:
+        if not run_all_test_scripts_parallely(sys.argv[1], sys.argv[2]):
+            exit(1)
+    else:
+        if not run_all_test_scripts_sequentially(sys.argv[1], sys.argv[2]):
+            exit(1)
 
 
 def install_dependencies():
@@ -28,7 +37,7 @@ def install_dependencies():
                            MONGODB_PACKAGE])
 
 
-def run_all_test_scripts(directory, api_token) -> bool:
+def run_all_test_scripts_parallely(directory, api_token) -> bool:
     test_processes = {}
 
     for root, _, files in os.walk(directory):
@@ -56,6 +65,33 @@ def run_all_test_scripts(directory, api_token) -> bool:
         else:
             is_all_ok = False
             print(f"[FAIL] {filepath}")
+
+    return is_all_ok
+
+
+def run_all_test_scripts_sequentially(directory, api_token) -> bool:
+    is_all_ok = True
+
+    for root, _, files in os.walk(directory):
+        for filename in files:
+            if not filename.endswith(".py"):
+                continue
+
+            filepath = os.path.join(root, filename)
+            process = subprocess.Popen(["python3", filepath, api_token],
+                                       stdout=sys.stdout,
+                                       stderr=sys.stderr)
+
+            if process.poll() is None:
+                result_code = process.wait()
+            else:
+                result_code = process.returncode
+
+            if result_code == 0:
+                print(f"[OK] {filepath}")
+            else:
+                is_all_ok = False
+                print(f"[FAIL] {filepath}")
 
     return is_all_ok
 
